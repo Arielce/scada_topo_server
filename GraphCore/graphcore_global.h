09db4724e51f441f5c2c965c709ea30887550c71
gewenlin <rover5269@icloud.com>
2018-03-19 23:33:46 +0800

    添加pro文件

---
+++ GraphCore/graphcore_global.h
@@ -0,0 +1,10 @@
+#ifndef GRAPHCORE_GLOBAL_H
+#define GRAPHCORE_GLOBAL_H
+
+#include <QtCore/qglobal.h>
+#include "CGraph.h"
+#include "CScadaAlg.h"
+#include "CTopoAlg.h"
+#include "CCommunityDetectAlg.h"
+
+#endif // GRAPHCORE_GLOBAL_H
